1070509a93ad9a87ea5ec86133f5fb788d0ce158
Gan Zhen Hao <ganzh6880@gmail.com>
2019-02-22 20:29:39 +0800

    Add a running chatbot!!! Yay!!

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-console.log("Hello World");
-const API_KEY = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I";
+console.log("Running Bot");
+const Telegraf = require('telegraf'); //import lib
+
+const API_KEY = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I"; //Authentication
+
+const bot = new Telegraf(API_KEY); //Create new bot
+
+bot.help(ctx => ctx.reply("Sorry can't help! Bye!")); //same as const Help=ctx => ctx.reply("Hello"); bot.help(Help)
+
+bot.command('hello', ctx => ctx.reply("Hello there!"))
+bot.launch();
+
+
+
 
