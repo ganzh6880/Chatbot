54e8abe3c96c68a4aceb9fbd3999483742d46a33
Gan Zhen Hao <ganzh6880@gmail.com>
2019-03-01 21:29:41 +0800

    Heroku Deploy

--- TDD.test.js
+++ TDD.test.js
@@ -1,6 +1,9 @@
+const CoffeeManager = require("./coffeeManager");
 describe("sanity",() =>{
     it('should pass',() => { 
         expect(0).toEqual(0);
         expect(4-2).toEqual(2);
     });
 });
+
+//module.export = CoffeeManager (export objects)
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+
 console.log("Running Bot");
 const Telegraf = require('telegraf'); //import lib
+const URL = process.env.URL || 'https://garagechatbot.herokuapp.com';
+const PORT = process.env.PORT || 3000;
 
 const API_KEY = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I"; //Authentication
 
 const bot = new Telegraf(API_KEY); //Create new bot
+bot.telegram.setWebhook(`${URL}/bot${API_KEY}`);
+bot.startWebhook(`/bot${API_KEY}`, null, PORT);
 
 bot.help(ctx => ctx.reply("Sorry can't help! Bye!")); //same as const Help=ctx => ctx.reply("Hello"); bot.help(Help)
 
 
 bot.hears(/hello/i, ctx => ctx.reply('Hello there!')); // /hello/i = (/../i: Regular Experession+case-sensitive)
 bot.launch();
+
 
 
 
