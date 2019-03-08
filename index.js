4579be00d1565b8871945b313cc416b15161de78
Gan Zhen Hao <ganzh6880@gmail.com>
2019-03-08 10:10:22 +0800

    debug commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const bot = new Telegraf(API_KEY); //Create new bot
 
 bot.help(ctx => ctx.reply("Sorry can't help! Bye!")); //same as const Help=ctx => ctx.reply("Hello"); bot.help(Help)
 
-
 bot.hears(/hello/i, ctx => ctx.reply('Hello there!')); // /hello/i = (/../i: Regular Experession+case-sensitive)
 
 bot.start(ctx => ctx.reply("Hello"));
