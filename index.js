9cdf81b12d5c50b03f586b8d7c0a2389d2ed0a8f
Gan Zhen Hao <ganzh6880@gmail.com>
2019-03-08 10:15:31 +0800

    add debug trace log

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-console.log("Running Bot");
-
 const Telegraf = require('telegraf'); //import lib
 
 const URL = process.env.URL || 'https://garagechatbot.herokuapp.com';
 const PORT = process.env.PORT || 3000;
-const API_KEY = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I"; //Authentication
+const API_TOKEN = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I"; //Authentication
 
-const bot = new Telegraf(API_KEY); //Create new bot
+const bot = new Telegraf(API_TOKEN); //Create new bot
 
 bot.help(ctx => ctx.reply("Sorry can't help! Bye!")); //same as const Help=ctx => ctx.reply("Hello"); bot.help(Help)
 
@@ -15,8 +13,10 @@ bot.hears(/hello/i, ctx => ctx.reply('Hello there!')); // /hello/i = (/../i: Reg
 bot.start(ctx => ctx.reply("Hello"));
 
 if (process.env.NODE_ENV === "production") {
-  bot.telegram.setWebhook(`${URL}/bot${API_KEY}`);
-  bot.startWebhook(`/bot${API_KEY}`, null, PORT);
+  console.log("Running with webhook");
+  bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+  bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 } else {
+  console.log("Running locally");
   bot.launch();
 }
