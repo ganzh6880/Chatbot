81d6364a9f1f0610454bf6c48800d66bf962faf0
Gan Zhen Hao <ganzh6880@gmail.com>
2019-03-08 09:42:39 +0800

    Without Procfile

--- API_KEY.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I"; //Authentication
-
-module.exports = API_KEY;
--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 console.log("Running Bot");
+
 const Telegraf = require('telegraf'); //import lib
+
 const URL = process.env.URL || 'https://garagechatbot.herokuapp.com';
 const PORT = process.env.PORT || 3000;
+const API_KEY = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I"; //Authentication
 
-const APIKEY = require('./API_KEY');
-
-const bot = new Telegraf(APIKEY); //Create new bot
-bot.telegram.setWebhook(`${URL}/bot${APIKEY}`);
-bot.startWebhook(`/bot${APIKEY}`, null, PORT);
+const bot = new Telegraf(API_KEY); //Create new bot
 
 bot.help(ctx => ctx.reply("Sorry can't help! Bye!")); //same as const Help=ctx => ctx.reply("Hello"); bot.help(Help)
 
 
 bot.hears(/hello/i, ctx => ctx.reply('Hello there!')); // /hello/i = (/../i: Regular Experession+case-sensitive)
-bot.launch();
+
+bot.start(ctx => ctx.reply("Hello"));
+
+if (process.env.NODE_ENV === "production") {
+  bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+  bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
+} else {
+  bot.launch();
+}
 
 
 
