36c9f889ee1d2a672bf63c59ac65c5e0652be447
Gan Zhen Hao <ganzh6880@gmail.com>
2019-03-09 11:05:16 +0800

    Try Push from VSC

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const URL = process.env.URL || 'https://garagechatbot.herokuapp.com';
 const PORT = process.env.PORT || 3000;
 const API_TOKEN = "749566027:AAFof_Mw7NKfKfCxS4KCCdzJKT0GE0mNq9I"; //Authentication
 
-const bot = new Telegraf(API_TOKEN); //Create new bot
+const bot = new Telegraf(API_TOKEN);   //Create new bot
 
 bot.help(ctx => ctx.reply("Sorry can't help! Bye!")); //same as const Help=ctx => ctx.reply("Hello"); bot.help(Help)
 bot.hears(/hello/i, ctx => ctx.reply('Hello there!')); // /hello/i = (/../i: Regular Experession+case-sensitive)
